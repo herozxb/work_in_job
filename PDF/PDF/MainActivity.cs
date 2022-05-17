fe7d6ef3b698c595dbac681d90cf9f7ddfffb527
herozxb <herozxb@gmail.com>
2022-05-17 22:47:56 +0800

    add code to class

--- PDF/PDF/MainActivity.cs
+++ PDF/PDF/MainActivity.cs
@@ -14,9 +14,6 @@ using System.Collections.Generic;
 using Android.Graphics;
 using Android.Content;
 using AndroidX.RecyclerView.Widget;
-using iText.Kernel.Pdf;
-using iText.Layout;
-using iText.Layout.Element;
 using SkiaSharp;
 using SkiaSharp.Views.Android;
 using System.Text;
@@ -50,12 +47,160 @@ namespace PDF
 
     public class PDFCrossReferences
     {
-        private readonly List<long> Positions = new List<long>();
-        private readonly List<int> Revisions = new List<int>();
+        public string content;
+        MemoryStream memory_stream = new MemoryStream();
+        public List<string> xref = new List<string>();
+
+        private List<long> Positions = new List<long>();
+        private List<int> Revisions = new List<int>();
 
         public long GetObjectPosition(int ObjectIndex) => Positions[ObjectIndex];
 
         public int GetObjectRevision(int ObjectIndex) => Revisions[ObjectIndex];
+
+
+
+        public PDFCrossReferences(Stream stream)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            stream.CopyTo(this.memory_stream);
+            this.memory_stream.Position = 0;
+            this.content = Encoding.ASCII.GetString(this.memory_stream.ToArray());
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("==========ElapsedMilliseconds[to String]=====");
+            Console.WriteLine(elapsedMs);
+            Console.WriteLine(memory_stream.CanSeek);
+
+            string trailer_string = read_trailer(content);
+            string startxref = read_int(trailer_string, "startxref");
+            this.xref = read_xref(content, int.Parse(startxref), read_length(content, int.Parse(startxref)));
+            make_position();
+        }
+
+        public void make_position()
+        {
+            this.Positions = make_position(this.xref);
+        }
+        public void make_revision()
+        {
+            this.Revisions = make_revision(this.xref);
+        }
+
+        public List<long> make_position( List<string> xref )
+        {
+            List<long> position = new List<long>();
+            for (int i = 0; i < xref.Count; i++)
+            {
+                position.Add(long.Parse(xref[i].Split(" ")[0]));
+            }
+
+            return position;
+        }
+
+        public List<int> make_revision( List<string> xref)
+        {
+            List<int> revision = new List<int>();
+            for (int i = 0; i < xref.Count; i++)
+            {
+                revision.Add(int.Parse(xref[i].Split(" ")[1]));
+            }
+
+            return revision;
+        }
+
+        public string read_int(string content, string label)
+        {
+            string result = "";
+            int entry_position = content.IndexOf(label) + label.Length;
+            while (true)
+            {
+                result = result + content[entry_position];
+                entry_position++;
+
+                if (content[entry_position] == '\r' || content[entry_position] == ' ' || content[entry_position] == '/')
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public string read_length(string content, int index)
+        {
+            string result = "";
+            int entry_position = index + 6; // +6 jump the xref /r/n length
+            while (true)
+            {
+                if (content[entry_position] == '\r')
+                {
+                    break;
+                }
+                result = result + content[entry_position];
+                entry_position++;
+            }
+
+            return result;
+        }
+
+        public string read_xref_line(string content, int index)
+        {
+            string result = "";
+            int entry_position = index;
+            while (true)
+            {
+                if (content[entry_position] == '\r')
+                {
+                    break;
+                }
+                result = result + content[entry_position];
+                entry_position++;
+            }
+
+            return result;
+        }
+
+        public string read_obj_index(string content, string label)
+        {
+            string result = "";
+            int entry_position = content.IndexOf(label) + label.Length;
+            while (content[entry_position] != 'R')
+            {
+                result = result + content[entry_position];
+                entry_position++;
+            }
+
+            return result + 'R';
+        }
+        public string read_trailer(string content)
+        {
+            string result = "";
+            result = content.Split("trailer")[1];// time = 0.127 
+            return result;
+        }
+
+        public List<string> read_xref(string content, int index, string xref_length)
+        {
+
+            List<string> xref_list = new List<string>();
+
+            int entry_position = index + 6 + xref_length.Length + 2;
+
+            int length = int.Parse(xref_length.Split(" ")[1]);
+
+            for (int i = 0; i < length; i++)
+            {
+                string xref_line = read_xref_line(content, entry_position);
+                entry_position = entry_position + xref_line.Length + 2; //+2 is to jump the "/r/n"
+                xref_list.Add(xref_line);
+            }
+
+            return xref_list;
+        }
+
     }
 
     public class PDFTrailer
@@ -81,11 +226,21 @@ namespace PDF
         public PDFTrailer(MemoryStream ObjectStream, PDFCrossReferences References)
         {
             CrossReferences = References;
-            //Size = 1; //Get From Stream
-            //RootIndex = 0; //Get From Stream
-            //InfoIndex = 0; //Get From Stream
+
+            string trailer_string = CrossReferences.read_trailer(CrossReferences.content);
+
+            string size = CrossReferences.read_int(trailer_string, "/Size");
+            string root = CrossReferences.read_obj_index(trailer_string, "/Root");
+            string info = CrossReferences.read_obj_index(trailer_string, "/Info");
+
+            this.Size = int.Parse(size);
+            this.RootIndex = int.Parse(root.Split(" ")[0]);
+            this.InfoIndex = int.Parse(info.Split(" ")[0]);
+
             Initialize(ObjectStream);
         }
+
+
     }
 
     public abstract class PDFObject
@@ -449,19 +604,28 @@ namespace PDF
             //{
             //}
 
-            watch = System.Diagnostics.Stopwatch.StartNew();
-
             MemoryStream memory_stream = new MemoryStream();
             stream.CopyTo(memory_stream);
             string content = Encoding.ASCII.GetString(memory_stream.ToArray());
+
+
+            stream = assets.Open("sample_2.pdf");
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+            PDFCrossReferences pdf_cross_reference = new PDFCrossReferences(stream);
+            pdf_cross_reference.make_position();
 
             watch.Stop();
             elapsedMs = watch.ElapsedMilliseconds;
             Console.WriteLine("==========ElapsedMilliseconds[GetString]=====");
             Console.WriteLine(elapsedMs);
             Console.WriteLine(memory_stream.CanSeek);
+            Console.WriteLine(pdf_cross_reference.GetObjectPosition(0));
+
+
             //memory_stream.read
-            
+
 
 
             //stream_text.Seek(4940559,SeekOrigin.Begin);  //not support 
