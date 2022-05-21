5a73d92b63ba259fd1c61d41e5d3c0e2f347df97
herozxb <herozxb@gmail.com>
2022-05-21 12:37:14 +0800

    can read all stream

--- PDF/PDF/MainActivity.cs
+++ PDF/PDF/MainActivity.cs
@@ -40,6 +40,7 @@ namespace PDF
     public class Page
     {
         public Dictionary<string, string> Entries = new Dictionary<string, string>();
+        public string content;
     }
 
 
@@ -124,6 +125,8 @@ namespace PDF
 
             return result;
         }
+
+
 
         public string read_length(string content, int index)
         {
@@ -505,21 +508,8 @@ namespace PDF
             stream.CopyTo(memory_stream);
             string content = Encoding.ASCII.GetString(memory_stream.ToArray());
 
-
-
-
-            //stream = assets.Open("sample_2.pdf");
-            //PDFCrossReferences pdf_cross_reference = new PDFCrossReferences(stream);
-
-            //PDFTrailer pdf_trailer_object = new PDFTrailer(memory_stream, pdf_cross_reference);
-
-            //PDFPages pdf_pages = ((PDFCatalog)(pdf_trailer_object.Root)).Pages;
-
-
             string trailer_string = read_trailer(content);
-
             string startxref = read_int(trailer_string, "startxref");
-
 
             watch = System.Diagnostics.Stopwatch.StartNew();
             Dictionary<string, string> xref = read_xref(content, int.Parse(startxref), read_length(content, int.Parse(startxref)));
@@ -530,153 +520,53 @@ namespace PDF
             Console.WriteLine("==========ElapsedMilliseconds[read_xref]=====");
             Console.WriteLine(elapsedMs);
 
-
-            int line_number = int.Parse(xref["10230"].Split(" ")[0]);
+            /*
+            int line_number = int.Parse(xref["6942"].Split(" ")[0]);
 
             string pages_object = read_obj(content, line_number);
-
             int stream_start = line_number + search_position_from_content(pages_object, "stream") + "stream".Length+2;//2 is for "/r/n"
             int stream_end = line_number + search_position_from_content(pages_object, "endstream")-2; //2 is for "/r/n"
+            int length_stream = stream_end - stream_start;
 
-            Console.WriteLine("=================stream_start==================");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write(content[stream_start + i]);
-            }
-            Console.WriteLine("===========");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write(content[stream_end + i]);
-            }
-
-            stream = assets.Open("sample_2.pdf");
-            stream.CopyTo(memory_stream);
             memory_stream.Position = stream_start;
 
-            byte[] byte_stream = new byte[10000];
+            byte[] byte_stream = new byte[length_stream];
 
-            int length_stream = stream_end - stream_start;
-            Console.WriteLine("=============length_stream===============");//37806870454946541337636363631310493248321119810613606047779711410773110102111606047779711410710110032102971081151016262477897109101115325457525232483282477911711610810511010111532494849574832483282477710111697100971169732515351324832824765991141117011111410932515352324832824780971031011153250324832824783116114117991168411410110182111111116325356324832824784121112101476797116971081111034776971101034010111045858341477911711611211711673110116101110116115916060477311010211140671141019711611111458327280323232323277971101171029799116117114101114587369673232323277111100101108581158271664147834771848395806870654947791171161121171166711111010010511610511111073100101110116105102105101114401158271664147681011151167911711611211711680114111102105108101325153503248328247841211121014779117116112117116731101161011101164782101103105115116114121789710910140104116116112584747119119119469911110811111446111114103416262936262131011101001119810613545752523248321119810613606047681011151161153254575253324832826262131011101001119810613494849574832483211198106136060477010511411511632494849505232483282476711111711011632545447769711511632494849505232483282478412111210147791171161081051101011156262131011101001119810613515351324832111981061360604783117981161211121014788777647761011101031161043251575052478412111210147771011169710097116976262115116114101971091310606312011297991071011163298101103105110613463636334321051006134875377487711267101104105721221141018312278849912210799571003463621060120581201091121091011169732120109108110115581206134971001119810158110115581091011169747343212058120109112116107613465100111981013288778032671111141013252464845995149543252524650535157504944328311711032799911632484932504848543249555849525851573462103232326011410010258826870321201091081101155811410010261341041161161125847471191191194611951461111141034749575757474850475050451141001024511512111011697120451101153534621032323232323260114100102586810111599114105112116105111110321141001025897981111171166134341032323232323232323232323212010910811011558112100102613410411611611258474711011546971001119810146991111094711210010247494651473462================[Pages][start]=====================
-            //13107213714086911111959161262239522571479893143239126132801623211218410031144215377733168105123146150352546151991111185521181701182272920722915524919822762239155179197821392456510425419135245771152461069223142058922310711621911061782021292471211714121019813723218820270244187701381822552091041335824597631984139186874398418728212381292503074014130113177224624154254641182021389617213814213017222818722228221230622321597911191352551051912441112083790122212011181401314673222572291631729403158182872426115495144179182179114211118322471631591910720453132982913824524214244541001877039219171109891181341572221472361182442384122121577241281731552612392720865249369214202102225188225122431516423655205852431881591441781051998542431194105479220420219714514214176240206191167185641278714610224470823763181160177301571477512924592182121162176110147196228146188341734517017547909150362411092251302436118119731823624921621822613177121181711111991841079492181246922912319219223624314643103161161381817415582541592371801081561591011061561372112542636472544065861151788321115114911615713651122210717614922018240362247136171739121222187881141391651365118229178157231116722710318441182155208821972012410457159831871211232436411718925195128138144782035110712724610375502141518524441892301032264222916121018534158554021229223025473932041033100163821638384433552271032271292441422311701602101931581931549720216784857835791391926120148175175249149143164102022092208111593962519234119224228235549669177139144215462432419257803616823138120232243163244261571051697424722623525419221523238136210234962002019119120921424749206225104185223321823991612133087702544435923457402132002023117987672462431959613110338223919210821414619221821789404247381324138181451765258882011903722412421295424512670887022221010595227199323823824816067572132473523213614723611630107302414324925552311222418114117821815710618720223021639119165782233224111433266224572191311021371921431752482511075813014125118710223322416493662251701991571921882401102251471866687154235248635489145232251150932411711681142721215816365592421081456725181672514241171031911602442977237481731612072113857175151251985792238203197191116185393238255940119818223148912925515216241245188107128110118451741551012431612494519206981861991310101================[Pages][start]=====================
-
-            //x½U]oÓ0}ÿp¤:vü-MÖ"
-            //Ý	¡j¡d¥M ~ÿë¤Ñ6%ÝCSÈ¾=>çøæ8ÏöÕö&[WprUU¶þU<,«ªÜ] Çé¯oy¼È6Û"«¶e/~ª|é¼,«|z
-            //Ãñâé+ØüïaÀ¨\`þG¥Õ¦rTXïÂ çUÆ%K¸ÀÙÝÕµÒCJSp*5ìó0øð0¦¸èçIHoÂ×j¢V·ËSTb°iL-À`+Ñ5¤oÂ`DµlÂ-5T«o
-
+            
             memory_stream.Read(byte_stream, 0, length_stream);
 
-            Console.WriteLine(length_stream);
-            for (int i = 0; i < length_stream ; i++)
-            {
-                //byte_stream[i] = (byte)memory_stream.ReadByte();
-                //Console.Write((char)byte_stream[i]);
-            }
+            string sub_string = content.Substring(stream_start, length_stream);
+            Console.WriteLine("=========sub_string============");
+            Console.WriteLine(sub_string);
+            // Convert a C# string to a byte array  
+            byte[] bytes = Encoding.Default.GetBytes(sub_string);
+            Console.WriteLine(bytes.Length);
 
+            //jump 2 bytes of the stream
             byte[] cutinput = new byte[byte_stream.Length - 2];
             Array.Copy(byte_stream, 2, cutinput, 0, cutinput.Length);
 
             MemoryStream stream_output = new MemoryStream();
-
             using (MemoryStream compressStream = new MemoryStream(cutinput))
             using (DeflateStream decompressor = new DeflateStream(compressStream, CompressionMode.Decompress))
                 decompressor.CopyTo(stream_output);
-
             string output_result = Encoding.Default.GetString(stream_output.ToArray());
+
+            //*/
+
+            string output_result = read_content( memory_stream, content, xref, "6942");
 
             Console.WriteLine("==========output_result[start]===========");
             Console.WriteLine(output_result);
             Console.WriteLine("==========output_result[end]===========");
 
-            /*
-            using (MemoryStream decompressedStream = new MemoryStream(cutinput))
-            {
-                using (DeflateStream deflateStream = new DeflateStream(decompressedStream, CompressionMode.Decompress))
-                {
-                    deflateStream.CopyTo(decompressedStream);
-                    byte[] result = decompressedStream.ToArray();
-                    Console.WriteLine("============decompressedStream===============");
-                    for (int i = 0; i < result.Length; i++)
-                    {
-                        Console.Write(result[i]);
-                    }
-                }
-            }
-            //*/
-            /*
-
-            int line_number = int.Parse(xref["6942"].Split(" ")[0]);
-
-            string pages_object = read_obj(content, line_number);
-            string stream_object = read_stream(pages_object, 0);
-
-            Console.WriteLine(stream_object);
-
-            //byte[] byte_array = Decompress(System.Text.Encoding.UTF8.GetBytes(stream_object));
-            //byte[] byte_array = System.Text.Encoding.UTF8.GetBytes(stream_object);
 
 
-            byte[] byte_array = System.Text.Encoding.UTF8.GetBytes(stream_object);
-            Console.WriteLine("==========DeflateStream[start]===========");
-            Console.WriteLine(byte_array.Length);
-
-            byte[] cutinput = new byte[byte_array.Length - 2];
-            Array.Copy(byte_array, 2, cutinput, 0, cutinput.Length);
-
-            var stream_output = new MemoryStream();
-
-            using (var compressStream = new MemoryStream(cutinput))
-            using (var decompressor = new DeflateStream(compressStream, CompressionMode.Decompress))
-            {
-                decompressor.CopyTo(stream_output);
-                Console.WriteLine("==========DeflateStream[start]===========");
-                Console.WriteLine(stream_output.Length);
-                Console.WriteLine("==========DeflateStream[end]===========");
-                for (int i = 0; i < stream_output.Length; i++)
-                {
-                    Console.Write(stream_output.ReadByte());
-
-                }
-            }
-                    
-
-
-
-
-            using (MemoryStream decompressedStream = new MemoryStream())
-            {
-                using (Stream compressStream = GenerateStreamFromString(stream_object))
-                {
-                    using (MemoryStream memory_stream_inside = new MemoryStream())
-                    {
-                        compressStream.CopyTo(memory_stream_inside);
-                        Console.WriteLine("==========[start][1]==========");
-                        Console.WriteLine(memory_stream_inside.Length);
-                        Console.WriteLine("==========[end][1]==========");
-
-                        using (DeflateStream deflateStream = new DeflateStream(memory_stream_inside, CompressionMode.Decompress))
-                        {
-                            deflateStream.CopyTo(decompressedStream);
-                            Console.WriteLine("==========[start]==========");
-                            Console.WriteLine(deflateStream.ReadByte());
-                            for (int i = 0; i < decompressedStream.ToArray().Length; i++)
-                            {
-                                Console.Write(decompressedStream.ToArray()[i]);
-                                Console.Write(decompressedStream.Length);
-                            }
-
-                            Console.WriteLine("==========[end]==========");
-                        }
-                    }
-                }
-
-
-            }
-
-            //*/
+            
+            
+            Pages complete_pages = make_pages( memory_stream, content, xref, "2");
 
             /*
-            Pages complete_pages = make_pages(content, xref, "2");
-
             string text = "";
 
             visit_tree_node(complete_pages, ref text);
@@ -968,7 +858,86 @@ namespace PDF
             return line_result;
         }
 
-        public Pages make_pages(string content, Dictionary<string, string> xref, string object_index)
+        public string clean_front_empty_space(string content)
+        {
+            while (content[0] == ' ')
+            {
+                content = content.Remove(0, 1);
+            }
+
+            return content;
+        }
+        public string read_stream_length(string content, string length_tag)
+        {
+            string result = "";
+            int entry_position = content.IndexOf(length_tag) + length_tag.Length; // +6 jump the "xref/r/n" length
+            while (true)
+            {
+                if (content[entry_position] == '>' || content[entry_position] == '/')
+                {
+                    break;
+                }
+                result = result + content[entry_position];
+                entry_position++;
+            }
+
+            return result;
+
+        }
+
+        public string read_content(MemoryStream memory_stream, string content, Dictionary<string,string> xref, string object_index)
+        {
+
+            int line_number = int.Parse(xref[object_index].Split(" ")[0]);
+
+            Console.WriteLine("==========line_number==========");
+            Console.WriteLine(line_number);
+
+            string pages_object = read_obj(content, line_number);
+
+            string stream_length = read_stream_length(pages_object, "/Length");
+
+            Console.WriteLine(pages_object);
+            Console.WriteLine(stream_length);
+
+            if (int.Parse(stream_length) > 0)
+            {
+
+
+                int stream_start = line_number + search_position_from_content(pages_object, "stream") + "stream".Length + 2;//2 is for "/r/n"
+                int stream_end = line_number + search_position_from_content(pages_object, "endstream") - 2; //2 is for "/r/n"
+                int length_stream = stream_end - stream_start;
+
+                memory_stream.Position = stream_start;
+
+                byte[] byte_stream = new byte[length_stream];
+
+
+                memory_stream.Read(byte_stream, 0, length_stream);
+
+                //jump 2 bytes of the stream 
+                byte[] cutinput = new byte[byte_stream.Length - 2];
+                Array.Copy(byte_stream, 2, cutinput, 0, cutinput.Length);
+
+                MemoryStream stream_output = new MemoryStream();
+                using (MemoryStream compressStream = new MemoryStream(cutinput))
+                using (DeflateStream decompressor = new DeflateStream(compressStream, CompressionMode.Decompress))
+                    decompressor.CopyTo(stream_output);
+                string output_result = Encoding.Default.GetString(stream_output.ToArray());
+
+                Console.WriteLine("==========output_result[start]===========");
+                Console.WriteLine(object_index);
+                Console.WriteLine(output_result);
+                Console.WriteLine("==========output_result[end]===========");
+
+                return output_result;
+            }
+
+            return null;
+
+        }
+
+        public Pages make_pages(MemoryStream memory_stream, string content, Dictionary<string, string> xref, string object_index)
         {
 
             Pages pages = new Pages();
@@ -1008,7 +977,7 @@ namespace PDF
                     //Console.WriteLine("================Kids[end]=====================");
                     if (kids_index.Replace(" ", "").Length > 0)
                     {
-                        pages.pages_tree_children_node_list.Add(make_pages(content, xref, kids_index));
+                        pages.pages_tree_children_node_list.Add(make_pages(memory_stream,content, xref, kids_index));
                     }
                 }
             }
@@ -1021,8 +990,12 @@ namespace PDF
                 Page leaf_node_page = new Page();
                 leaf_node_page.Entries.Add("/Parent", read_obj_index(pages_object, "/Parent"));
                 leaf_node_page.Entries.Add("/MediaBox", read_array(pages_object, "/MediaBox"));
-                leaf_node_page.Entries.Add("/Contents", read_obj_index(pages_object, "/Contents"));
+                string content_object = read_obj_index(pages_object, "/Contents");
+                content_object = clean_front_empty_space(content_object);
+                leaf_node_page.Entries.Add("/Contents", content_object);
                 Console.WriteLine(leaf_node_page.Entries["/Parent"]);
+                leaf_node_page.content = read_content(memory_stream, content, xref, content_object.Split(" ")[0]);
+                Console.WriteLine(leaf_node_page.content);
 
                 pages_end.page_leaf_node = leaf_node_page;
                 pages.pages_tree_children_node_list.Add(pages_end);
